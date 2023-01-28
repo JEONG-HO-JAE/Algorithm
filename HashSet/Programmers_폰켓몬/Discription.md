c1a1fa06bc94bcb089ba5e8bbe078c3f473b6120
EddieJeong <98eddiechung@gmail.com>
2023-01-28 18:03:59 +0900

    Programmers

---
+++ HashSet/Programmers_폰켓몬/Discription.md
@@ -0,0 +1,5 @@
+# Source 
+https://school.programmers.co.kr/learn/courses/30/lessons/1845<br>
+
+## Short dicription
+Using HashSet Data Structure
---
+++ HashSet/Programmers_폰켓몬/PG_PocketMon.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+public class PG_PocketMon {
+}
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        Set <Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            set.add(nums[i]);
+        }
+        if(set.size() <= nums.length/2) answer = set.size();
+        else answer = nums.length/2;
+
+        return answer;
+    }
+}
